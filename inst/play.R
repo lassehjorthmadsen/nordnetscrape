19f0587164aa923c6a06bbd2c3e41c1be4372263
LMDN <lmdn@novonordisk.com>
2021-12-30 22:21:07 +0100

    added price data set file, to avoid big download

--- inst/play.R
+++ inst/play.R
@@ -8,24 +8,36 @@ load_all()
 theme_set(theme_minimal() +
             theme(strip.text = element_text(size = 7, hjust = 0)))
 
-omx <- read.csv2("data/omxc25.csv", check.names = F)
+omxc25_companies <- read.csv2("data/omxc25.csv", check.names = F)
 
 date_t      <-  Sys.Date()
 date_t_1    <-  Sys.Date() - 365
 
-# prices from Yahoo
-omxPrices <- omx$Symbol %>%
-  map(getSymbols, from = "2015-01-01", to = date_t, auto.assign = FALSE)
+omx_prices_file <- "data-raw/omx_prices.rds"
 
-omxClean <- omxPrices %>%
-  map(as.data.frame) %>%
-  map(rownames_to_column, "date") %>%
-  map(as_tibble) %>%
-  map_dfr(pivot_longer, -date) %>%
-  mutate(date = as.Date(date)) %>%
-  separate(name, into = c("company", "metric"), sep = ".CO.") %>%
-  mutate(company = paste0(company, ".CO")) %>%
-  left_join(omx, c("company" = "Symbol"))
+if (file.exists(omx_prices_file)) {
+
+  omx_prices <- readRDS(omx_prices_file)
+
+  } else {
+
+  # prices from Yahoo
+  omx_prices <- omxc25_companies$Symbol %>%
+    map(getSymbols, from = "2015-01-01", to = date_t, auto.assign = FALSE)
+
+  omx_clean <- omx_prices %>%
+    map(as.data.frame) %>%
+    map(rownames_to_column, "date") %>%
+    map(as_tibble) %>%
+    map_dfr(pivot_longer, -date) %>%
+    mutate(date = as.Date(date)) %>%
+    separate(name, into = c("company", "metric"), sep = ".CO.") %>%
+    mutate(company = paste0(company, ".CO")) %>%
+    left_join(omxc25_companies, c("company" = "Symbol"))
+
+  saveRDS(omx_clean, omx_prices_file)
+}
+
 
 
 mPrice  <-  getSymbols("NOVO-B.CO", from = "2015-11-28", to = date_t, auto.assign = FALSE)
@@ -62,7 +74,7 @@ price3 <- ragtop::blackscholes(callput = 1,
                                dividends = NULL)
 
 # Plot
-omxClean %>%
+omx_clean %>%
   filter(str_detect(metric, "Adjusted")) %>%
   ggplot(aes(x = date, y = value, color = `Company Name`)) +
   geom_line(show.legend = F) +
